2ce0f05407c4c52c7fc9265c08c1b8cd0dfb993c
Kavindu9 <kavinduliyo@gmail.com>
2026-05-09 11:04:29 +0500

    new Stay template

--- theme/index.php
+++ theme/index.php
@@ -18,10 +18,7 @@ get_header();
 	<section id="primary">
 		<main id="main">
 
-		
-	<h1 class="text-3xl font-bold underline text-clifford">
-      Hello world! Testing WEBSITE....
-    </h1>
+
 
 		<?php
 		if ( have_posts() ) {
---
+++ theme/page-stay.php
@@ -0,0 +1,36 @@
+<?php 
+/* 
+    Template Name: Stay Page
+*/ 
+?>
+
+<?php get_header(); ?>
+<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
+
+
+
+<section class="lg:py-12 py-10">
+    <div class="container max-w-screen-sm px-4 lg:px-0  mx-auto py-4"> 
+	
+        <h1 class="text-3xl font-bold underline text-clifford">
+        Hello world! Testing WEBSITE....
+        </h1>
+    </div>
+	
+</section>
+
+
+<?php endwhile; ?>
+<?php else : ?>
+
+<!-- article -->
+<article>
+
+    <h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+</article>
+<!-- /article -->
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
